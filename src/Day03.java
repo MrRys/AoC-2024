e0802b066538c8f2fd8a0e1448bf4d3805d9a4c1
MrRys <Richard.letanec@gmail.com>
2024-12-03 10:57:41 +0100

    Day03 Java Solution

--- src/Day03.java
+++ src/Day03.java
@@ -1,8 +1,12 @@
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day03 extends Day {
 
     final static private String inputFile = "inputs/day03.txt";
+
+    private String memory;
 
     public Day03() {
 
@@ -22,16 +26,34 @@ public class Day03 extends Day {
     }
 
     void parseInput() {
-
+        this.memory = String.join("", this.getInput());
     }
 
     public long part1() {
         long result = 0;
+        Matcher matcher = Pattern.compile("mul\\(([0-9]+),([0-9]+)\\)").matcher(this.memory);
+        while (matcher.find()) {
+            result += Long.parseLong(matcher.group(1)) * Long.parseLong(matcher.group(2));
+        }
         return result;
     }
 
     public long part2() {
         long result = 0;
+        boolean enabled = true;
+
+        Matcher matcher = Pattern.compile("mul\\(([0-9]+),([0-9]+)\\)|(do\\(\\))|(don't\\(\\))").matcher(this.memory);
+        while (matcher.find()) {
+            if (enabled && matcher.group(1) != null && matcher.group(2) != null) {
+                result += Long.parseLong(matcher.group(1)) * Long.parseLong(matcher.group(2));
+            }
+            if (matcher.group(3) != null) {
+                enabled = true;
+            }
+            if (matcher.group(4) != null) {
+                enabled = false;
+            }
+        }
         return result;
     }
 }
