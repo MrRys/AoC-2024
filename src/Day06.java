bef662b3608fe34d726d1b9ca4704395e6e45846
MrRys <Richard.letanec@gmail.com>
2024-12-06 13:13:26 +0100

    Day06 minor refactor

--- src/Day06.java
+++ src/Day06.java
@@ -127,27 +127,27 @@ public class Day06 extends Day {
     }
 
     public long part1() {
-        long result = 0;
-
         resetGrid();
+
+        long result = setWalkedTile();
 
         while (isInGrid()) {
             if (canMove()) {
-                result += setWalkedTile();
                 moveGuard();
+                result += setWalkedTile();
                 continue;
             }
 
             changeGuardDirection();
         }
 
-        return result + setWalkedTile();
+        return result;
     }
 
     public long part2() {
-        long result = 0;
-
         resetGrid();
+
+        long result = 0;
 
         for (int row = 0; row < workGrid.length; row++) {
             for (int col = 0; col < workGrid[0].length; col++) {
